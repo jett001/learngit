f07ef00449fc9a7d96a6a3f1471f5008bd69ff99
wangwenjiehlw <wangwenjiehlwrjeb@bbktel.com>
2017-07-28 10:01:16 +0800

    用git diff比较

--- source/Test.java
+++ source/Test.java
@@ -4,7 +4,13 @@
 * @date 2017年7月27日 下午4:16:05
 */
 public class Test {
+	private String name;
+	
 	public static void main(String[] args) {
 		System.out.println("hi git!");
+	}
+	
+	public void f1(){
+		return ;
 	}
 }
